cdaf894a1117422408ed043342b2f4c62f9b4f4e
Brian Black <black@gatech.edu>
2016-05-18 21:33:26 -0400

    2d path model

--- client/drawer.js
+++ client/drawer.js
@@ -13,31 +13,70 @@ CanvasDrawer.prototype.worldCoordsToCanvasCoords = function(worldCoords){
   return [worldCoords[0], this.canvas.height - worldCoords[1]];
 }
 
+function dot(v1, v2){
+  return v1[0]*v2[0] + v1[1]*v2[1];
+}
+
+function vectorTimesMatrix(v, m){
+  return [dot(v, m[0]), dot(v, m[1])];
+}
+
+function vectorPlusVector(v1, v2){
+  return [v1[0] + v2[0], v1[1] + v2[1]]
+}
+
+CanvasDrawer.draw = {
+  player: function(cd, e){
+    var ctx = cd.canvas.getContext('2d');
+    var model = [[0, 0], [-5, 10], [15, 0], [-5, -10], [0, 0]];
+    var rotMatrix = [
+      [Math.cos(e.orientAngle), -Math.sin(e.orientAngle)],
+      [Math.sin(e.orientAngle), Math.cos(e.orientAngle)]
+    ];
+    ctx.beginPath();
+    ctx.strokeStyle = e.color;
+    _.each(model, (vert) => {
+      var vertRot = vectorTimesMatrix(vert, rotMatrix);
+      var vertWorld = vectorPlusVector(vertRot, e.position);
+      var vertCanvas = cd.worldCoordsToCanvasCoords(vertWorld);
+      ctx.lineTo(vertCanvas[0], vertCanvas[1]);
+    });
+    ctx.stroke();
+    ctx.closePath();
+  }
+};
+
+CanvasDrawer.drawMinimumEntity = function(cd, e){
+  var ctx = cd.canvas.getContext('2d');
+  var canvasCoords = cd.worldCoordsToCanvasCoords(e.position);
+  var x = canvasCoords[0];
+  var y = canvasCoords[1];
+  ctx.beginPath();
+  ctx.arc(x, y, 5, 0, 2*Math.PI);
+  ctx.fillStyle = e.color || 'white';
+  ctx.fill();
+  ctx.closePath();
+  // dir indicator
+  if (e.hasOwnProperty('orientAngle')) {
+    ctx.beginPath();
+    var dirIndWorldCoords = [
+      e.position[0] + 8*Math.cos(e.orientAngle),
+      e.position[1] + 8*Math.sin(e.orientAngle)
+    ];
+    var dirIndCanvasCoords = cd.worldCoordsToCanvasCoords(dirIndWorldCoords);
+    ctx.arc(dirIndCanvasCoords[0], dirIndCanvasCoords[1], 2, 0, 2*Math.PI);
+    ctx.fillStyle = e.color || 'white';
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
 CanvasDrawer.prototype.draw = function(frame){
   var ctx = this.canvas.getContext('2d');
   ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   _.each(frame.entities, (e) => {
-    var canvasCoords = this.worldCoordsToCanvasCoords(e.position);
-    var x = canvasCoords[0];
-    var y = canvasCoords[1];
-    ctx.beginPath();
-    ctx.arc(x, y, 5, 0, 2*Math.PI);
-    ctx.fillStyle = e.color || 'white';
-    ctx.fill();
-    ctx.closePath();
-    // dir indicator
-    if (e.hasOwnProperty('orientAngle')) {
-      ctx.beginPath();
-      var dirIndWorldCoords = [
-        e.position[0] + 8*Math.cos(e.orientAngle),
-        e.position[1] + 8*Math.sin(e.orientAngle)
-      ];
-      var dirIndCanvasCoords = this.worldCoordsToCanvasCoords(dirIndWorldCoords);
-      ctx.arc(dirIndCanvasCoords[0], dirIndCanvasCoords[1], 2, 0, 2*Math.PI);
-      ctx.fillStyle = e.color || 'white';
-      ctx.fill();
-      ctx.closePath();
-    }
+    var drawEnt = CanvasDrawer.draw[e.type] || CanvasDrawer.drawMinimumEntity;
+    drawEnt(this, e);
   })
 }
 
