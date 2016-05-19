883c75c33f395cd520c07476672f93786e64661f
Brian Black <black@gatech.edu>
2016-05-19 15:22:45 -0400

    remove unused CanvasDrawer.drawEntity

--- client/drawer.js
+++ client/drawer.js
@@ -80,42 +80,6 @@ CanvasDrawer.prototype.draw = function(frame){
   })
 }
 
-CanvasDrawer.prototype.drawEntity = function(canvas, e){
-  var canvasCoords = this.worldCoordsToCanvasCoords(e.position);
-  var x = canvasCoords[0];
-  var y = canvasCoords[1];
-  ctx.beginPath();
-  ctx.arc(x, y, 5, 0, 2*Math.PI);
-  ctx.fillStyle = e.color || 'white';
-  ctx.fill();
-  ctx.closePath();
-}
-
-CanvasDrawer.prototype.drawEntity.player = function(canvas, e){
-  var ctx = this.canvas.getContext('2d');
-  var canvasCoords = this.worldCoordsToCanvasCoords(e.position);
-  var x = canvasCoords[0];
-  var y = canvasCoords[1];
-  ctx.beginPath();
-  ctx.arc(x, y, 5, 0, 2*Math.PI);
-  ctx.fillStyle = e.color || 'white';
-  ctx.fill();
-  ctx.closePath();
-  // dir indicator
-  if (e.hasOwnProperty('orientAngle')) {
-    ctx.beginPath();
-    var dirIndWorldCoords = [
-      e.position[0] + 8*Math.cos(e.orientAngle),
-      e.position[1] + 8*Math.sin(e.orientAngle)
-    ];
-    var dirIndCanvasCoords = this.worldCoordsToCanvasCoords(dirIndWorldCoords);
-    ctx.arc(dirIndCanvasCoords[0], dirIndCanvasCoords[1], 2, 0, 2*Math.PI);
-    ctx.fillStyle = e.color || 'white';
-    ctx.fill();
-    ctx.closePath();
-  }
-}
-
 CanvasDrawer.prototype.drawAndRepeat = function(){
   this.draw();
   window.requestAnimationFrame(() => {
